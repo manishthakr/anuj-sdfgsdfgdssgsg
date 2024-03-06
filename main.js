e96ec99783b7bc19cf40ec94f27863052c00f1f9
manish thakur <emailmanishthakur@gmail.com>
2024-03-06 15:25:29 +0545

    first commit

---
+++ main.js
@@ -0,0 +1,75 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var header = document.getElementById('myHeader');
+  var page = document.getElementById('page');
+  var openMenuButton = document.getElementById('openmenu');
+
+  window.addEventListener('scroll', function() {
+      page.classList.remove('menuopen');
+      if (window.scrollY >= 100) {
+          header.classList.add('sticky');
+      } else {
+          header.classList.remove('sticky');
+      }
+  });
+
+  // Event listener to remove the sticky class when the button is clicked
+  openMenuButton.addEventListener('click', function() {
+      header.classList.remove('sticky');
+      page.classList.add('menuopen');
+  });
+
+var links = document.querySelectorAll('a[href^="#"]');
+
+  links.forEach(function(link) {
+      link.addEventListener('click', function(event) {
+          // Prevent the default action
+          event.preventDefault();
+
+          // Get the target element
+          var targetId = this.getAttribute('href');
+          var targetElement = document.querySelector(targetId);
+
+          // Smooth scroll to target
+          if (targetElement) {
+              targetElement.scrollIntoView({
+                  behavior: 'smooth'
+              });
+          }
+      });
+  });
+});
+
+const labels = document.querySelectorAll('.form-control label');
+
+labels.forEach(label => {
+    label.innerHTML = label.innerText
+        .split('')
+        .map((letter, index) => `<span style="transition-delay:${index * 50}ms">${letter}</span>`)
+        .join('');
+});
+
+// Get the textarea element
+const textarea = document.getElementById('userInput');
+
+// Listen for input events (keyup or paste)
+textarea.addEventListener('input', () => {
+    const userInput = textarea.value.trim(); // Remove leading/trailing spaces
+    const wordCount = userInput.split(/\s+/).length; // Count words
+
+    // Calculate the shade of red based on word count
+    const maxWords = 10;
+    const redIntensity = Math.min(255, Math.floor((wordCount / maxWords) * 255));
+    const backgroundColor = `rgb(${redIntensity}, 0, 0)`;
+
+    // Set the background color
+    textarea.style.backgroundColor = backgroundColor;
+});
+
+
+
+
+       // Function to toggle the "clicked" class on the icon
+       function rotateIcon() {
+        const icon = document.querySelector('.bx-reset');
+        icon.classList.toggle('clicked');
+    }
